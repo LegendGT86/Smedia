8fe1c411192d1b0bdf5b1e719a4e73b69107a004
Dylan Matheson <dmatheson23@gmail.com>
2023-07-25 16:24:39 +0200

    django user updates

--- socialapp/admin.py
+++ socialapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 
-# Register your models here.
+# unRegister your models here.
+admin.site.unregister(Group)
+
+#extend user model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Just display username field on admin
+    fields = ["username"]
+
+    #Unregister innitial user
+admin.site.unregister(User)
+    #Reregister user
+admin.site.register(User, UserAdmin)
+                         
--- socialapp/models.py
+++ socialapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#creating A User Profile Model
+#This will associate one user to one profile, on delete all fields cascaded below will be deleted
+#one profile can follow many, symettrical in that you can follow them, they dont have to follow you back
+#Blank true meaning if you decide, you dont have to follow anyone
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField("self",
+                                     related_name="followed_by",
+                                     symmetrical=False,
+                                     blank=True)
+
