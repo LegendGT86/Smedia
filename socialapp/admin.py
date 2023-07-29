91ab1d66e32d689296b4a94682c41defb1dc4b25
Dylan Matheson <dmatheson23@gmail.com>
2023-07-29 11:16:26 +0200

    website profile build

--- socialapp/admin.py
+++ socialapp/admin.py
@@ -1,17 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
+from .models import Profile
 
 # unRegister your models here.
 admin.site.unregister(Group)
 
+#Embed profile into User on Django admin
+class ProfileMerge(admin.StackedInline):
+    model = Profile  
+                        
 #extend user model
 class UserAdmin(admin.ModelAdmin):
     model = User
     # Just display username field on admin
     fields = ["username"]
+    inlines = [ProfileMerge]
 
-    #Unregister innitial user
+#Unregister innitial user
 admin.site.unregister(User)
-    #Reregister user
+
+#Reregister user and profile
 admin.site.register(User, UserAdmin)
-                         
+#admin.site.register(Profile)
