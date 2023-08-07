0c6f22f7a916736e9e87ca56cb128496215c5419
Dylan Matheson <dmatheson23@gmail.com>
2023-08-07 20:30:12 +0200

    user registrations

--- socialapp/views.py
+++ socialapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from .models import Profile, Tweet
-from .forms import TweetForm
+from .forms import TweetForm, SignUpForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 # Create your views here.
 #currently this enables user logged in to see all tweets but cannot tweet from account if not logged in
@@ -27,7 +30,7 @@ def profile_list(request):
         profiles = Profile.objects.exclude(user=request.user)
         return render(request, 'profile_list.html', {"profiles":profiles})
     else:
-        messages.success(request, ("Sorry but you must be logged in to view this page"))
+        messages.warning(request, ("Sorry but you must be logged in to view this page"))
         return redirect('home')
     
 def profile(request, pk):
@@ -50,5 +53,44 @@ def profile(request, pk):
             current_user_profile.save()    
         return render(request, "profile.html",{"profile":profile, "tweets" : tweets})
     else:
-        messages.success(request, ("Sorry but you must be logged in to view this page"))
+        messages.warning(request, ("Sorry but you must be logged in to view this page"))
         return redirect('home')
+    
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("Congrads, you have been successfully logged in"))
+            return redirect('home')
+        
+        else:
+            messages.error(request, ("Username or Password is incorrect"))
+            return redirect('login')
+    else:
+        return render(request, "login.html",{})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have successfully signed out, bye for now"))
+    return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid:
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            #Login User
+            user = authenticate(username = username, password=password)
+            login(request,user)
+            messages.success(request, (f'You have successfully registered, Welcome {first_name}'))
+            return redirect('home')
+    return render(request, "register.html",{'form':form})
