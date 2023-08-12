062830d34f8b844ab1a4265df5566dec03cdc57b
Dylan Matheson <dmatheson23@gmail.com>
2023-08-12 20:19:04 +0200

    user profile pics

--- socialapp/models.py
+++ socialapp/models.py
@@ -31,6 +31,8 @@ class Profile(models.Model):
     
     #auto_now gives us a date last modified
     date_modified = models.DateTimeField(User, auto_now= True)
+    profile_image = models.ImageField(null=True, blank=True, upload_to="images/")
+    
     def __str__(self):
         return self.user.username
 
